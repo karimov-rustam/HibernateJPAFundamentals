637b1127a5b85f7791f695bac3a9736bafdce08d
Karimov R <rkarimov.r@gmail.com>
2018-01-16 21:59:05 +0400

    Implement simple hibernate sample

--- src/main/java/edu/hibernate/data/Application.java
+++ src/main/java/edu/hibernate/data/Application.java
@@ -1,8 +1,12 @@
 package edu.hibernate.data;
 
 import edu.hibernate.data.entities.AccountType;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Date;
 import java.util.Properties;
 
 public class Application {
@@ -18,8 +22,25 @@ public class Application {
                 put("hibernate.connection.driver_class",
                         "com.mysql.jdbc.Driver");
                 put("hibernate.connection.url",
-                        "jdbc:/mysql://localhost:3306/ifinances");
+                        "jdbc:mysql://localhost:3306/ifinances");
             }
         });
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory(
+                new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build());
+
+        Session session = sessionFactory.openSession();
+
+        session.beginTransaction();
+        AccountType type = new AccountType();
+        type.setName("Checking");
+        type.setCreatedDate(new Date());
+        type.setLastUpdatedDate(new Date());
+        type.setCreatedBy("rustamkarimov");
+        type.setLastUpdatedBy("rustamkarimov");
+        session.save(type);
+        session.getTransaction().commit();
+        session.close();
+
     }
 }
--- src/main/java/edu/hibernate/data/entities/AccountType.java
+++ src/main/java/edu/hibernate/data/entities/AccountType.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name = "ACCOUNT_TYPE")
+@Table(name = "account_type")
 public class AccountType {
 
     @Id
