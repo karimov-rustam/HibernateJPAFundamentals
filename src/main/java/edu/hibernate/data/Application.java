55eb6a452819a0a86a417c6d27b71627e9d31983
Karimov R <rkarimov.r@gmail.com>
2018-01-17 00:00:59 +0400

    Implement user entity mapping

--- src/main/java/edu/hibernate/data/Application.java
+++ src/main/java/edu/hibernate/data/Application.java
@@ -1,46 +1,30 @@
 package edu.hibernate.data;
 
-import edu.hibernate.data.entities.AccountType;
+import edu.hibernate.data.entities.User;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 
 import java.util.Date;
-import java.util.Properties;
 
 public class Application {
 
     public static void main(String[] args) {
-        Configuration configuration = new Configuration();
-        configuration.addAnnotatedClass(AccountType.class);
 
-        configuration.setProperties(new Properties() {
-            {
-                put("hibernate.connection.username", "lxuser");
-                put("hibernate.connection.password", "mypass");
-                put("hibernate.connection.driver_class",
-                        "com.mysql.jdbc.Driver");
-                put("hibernate.connection.url",
-                        "jdbc:mysql://localhost:3306/ifinances");
-            }
-        });
+        Session session = HibernateUtil.getSessionFactory().openSession();
 
-        SessionFactory sessionFactory = configuration.buildSessionFactory(
-                new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build());
+        session.getTransaction().begin();
 
-        Session session = sessionFactory.openSession();
+        User user = new User();
+        user.setBirthDate(new Date());
+        user.setCreatedDate(new Date());
+        user.setCreatedBy("Rustam");
+        user.setEmailAddress("test@test.com");
+        user.setFirstName("Rustam");
+        user.setLastName("Karimov");
+        user.setLastUpdateby("Rustam");
+        user.setLastUpdateDate(new Date());
 
-        session.beginTransaction();
-        AccountType type = new AccountType();
-        type.setName("Checking");
-        type.setCreatedDate(new Date());
-        type.setLastUpdatedDate(new Date());
-        type.setCreatedBy("rustamkarimov");
-        type.setLastUpdatedBy("rustamkarimov");
-        session.save(type);
+        session.save(user);
         session.getTransaction().commit();
         session.close();
-
     }
 }
---
+++ src/main/java/edu/hibernate/data/entities/User.java
@@ -0,0 +1,110 @@
+package edu.hibernate.data.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "finances_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
+    private Long userId;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "BIRTH_DATE")
+    private Date birthDate;
+
+    @Column(name = "EMAIL_ADDRESS")
+    private String emailAddress;
+
+    @Column(name = "LAST_UPDATED_DATE")
+    private Date lastUpdateDate;
+
+    @Column(name = "LAST_UPDATED_BY")
+    private String lastUpdateby;
+
+    @Column(name = "CREATED_DATE")
+    private Date createdDate;
+
+    @Column(name = "CREATED_BY")
+    private String createdBy;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public Date getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(Date lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public String getLastUpdateby() {
+        return lastUpdateby;
+    }
+
+    public void setLastUpdateby(String lastUpdateby) {
+        this.lastUpdateby = lastUpdateby;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+}
