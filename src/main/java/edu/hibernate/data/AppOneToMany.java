3bd7f010e1dcbf8c316aa5bd6739548070490fe6
Karimov R <rkarimov.r@gmail.com>
2018-01-24 12:38:16 +0400

    Implement @OneToMany unidirectional entity association

---
+++ src/main/java/edu/hibernate/data/AppOneToMany.java
@@ -0,0 +1,75 @@
+package edu.hibernate.data;
+
+import edu.hibernate.data.entities.Account;
+import edu.hibernate.data.entities.Transaction;
+import org.hibernate.Session;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class AppOneToMany {
+
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        try {
+            org.hibernate.Transaction transaction = session.beginTransaction();
+
+            Account account = createNewAccount();
+            account.getTransactions().add(createNewBeltPurchase());
+            account.getTransactions().add(createNewShoePurchase());
+            session.save(account);
+
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+            HibernateUtil.getSessionFactory().close();
+        }
+    }
+
+    private static Transaction createNewShoePurchase() {
+        Transaction shoePurchase = new Transaction();
+        shoePurchase.setTitle("Work Shoes");
+        shoePurchase.setAmount(new BigDecimal("100.00"));
+        shoePurchase.setClosingBalance(new BigDecimal("0.00"));
+        shoePurchase.setCreatedBy("Kevin Bowersox");
+        shoePurchase.setCreatedDate(new Date());
+        shoePurchase.setInitialBalance(new BigDecimal("0.00"));
+        shoePurchase.setLastUpdatedBy("Kevin Bowersox");
+        shoePurchase.setLastUpdatedDate(new Date());
+        shoePurchase.setNotes("Nice Pair of Shoes");
+        shoePurchase.setTransactionType("Debit");
+        return shoePurchase;
+    }
+
+    private static Transaction createNewBeltPurchase() {
+        Transaction beltPurchase = new Transaction();
+        beltPurchase.setTitle("Dress Belt");
+        beltPurchase.setAmount(new BigDecimal("50.00"));
+        beltPurchase.setClosingBalance(new BigDecimal("0.00"));
+        beltPurchase.setCreatedBy("Kevin Bowersox");
+        beltPurchase.setCreatedDate(new Date());
+        beltPurchase.setInitialBalance(new BigDecimal("0.00"));
+        beltPurchase.setLastUpdatedBy("Kevin Bowersox");
+        beltPurchase.setLastUpdatedDate(new Date());
+        beltPurchase.setNotes("New Dress Belt");
+        beltPurchase.setTransactionType("Debit");
+        return beltPurchase;
+    }
+
+    private static Account createNewAccount() {
+        Account account = new Account();
+        account.setCloseDate(new Date());
+        account.setOpenDate(new Date());
+        account.setCreatedBy("Kevin Bowersox");
+        account.setInitialBalance(new BigDecimal("50.00"));
+        account.setName("Savings Account");
+        account.setCurrentBalance(new BigDecimal("100.00"));
+        account.setLastUpdatedBy("Kevin Bowersox");
+        account.setLastUpdatedDate(new Date());
+        account.setCreatedDate(new Date());
+        return account;
+    }
+}
