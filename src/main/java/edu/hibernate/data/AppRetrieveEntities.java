01ecbfbf9ebfcb733f2ddd85890f7fc8aad8bc0b
Karimov R <rkarimov.r@gmail.com>
2018-01-26 00:42:28 +0400

    Add remove and modifying entities samples

--- src/main/java/edu/hibernate/data/AppRetrieveEntities.java
+++ src/main/java/edu/hibernate/data/AppRetrieveEntities.java
@@ -3,6 +3,8 @@ package edu.hibernate.data;
 import edu.hibernate.data.entities.Bank;
 import org.hibernate.Session;
 
+import java.util.Date;
+
 public class AppRetrieveEntities {
     public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
@@ -16,6 +18,14 @@ public class AppRetrieveEntities {
             Bank bank2 = (Bank) session.load(Bank.class, 1L);
             System.out.println("Method Executed");
             System.out.println(bank2.getName());
+
+            bank1.setName("Sberbank");
+            bank1.setLastUpdateBy("Rustam Karimov");
+            bank1.setLastUpdatedDate(new Date());
+
+            session.delete(bank1);
+            System.out.println("Bank1 Deleted!");
+            System.out.println(session.contains(bank1));
 
             transaction.commit();
 
