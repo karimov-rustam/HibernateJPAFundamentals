c3d79025f7a40ee77a9fe334c1138fe3331d35d0
Karimov R <rkarimov.r@gmail.com>
2018-01-29 00:16:48 +0400

    Add save, retrieve, modify and update entity JPA sample

--- src/main/java/edu/hibernate/data/AppJPAConfig.java
+++ src/main/java/edu/hibernate/data/AppJPAConfig.java
@@ -9,19 +9,33 @@ import javax.persistence.Persistence;
 
 public class AppJPAConfig {
     public static void main(String[] args) {
+
         EntityManagerFactory factory = null;
         EntityManager em = null;
         EntityTransaction tx = null;
 
-
         try {
-            factory = Persistence.createEntityManagerFactory("inifinite-finances");
+            factory = Persistence.createEntityManagerFactory("infinite-finances");
             em = factory.createEntityManager();
             tx = em.getTransaction();
             tx.begin();
 
+            //save entity
             Bank bank = MyUtil.createBank();
             em.persist(bank);
+
+            //retrieve entity
+            Bank dbBank = em.find(Bank.class, 1L);
+            System.out.println(em.contains(dbBank));
+            System.out.println(dbBank.getName());
+
+            Bank dbBank2 = em.getReference(Bank.class, 1234L);
+            System.out.println(em.contains(dbBank2));
+            System.out.println(dbBank2.getName());
+
+            dbBank.setName("Update Name");
+
+            em.remove(dbBank);
 
             tx.commit();
         } catch (Exception e) {
